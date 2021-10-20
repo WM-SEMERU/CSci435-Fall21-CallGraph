2f3dfa6b489ca06186a0875cd27f665ca12c3996
maxwelliam <mtward01@email.wm.edu>
2021-10-20 17:38:47 -0400

    refactored into methods and class, added comments

--- practice_parser/parser_python.py
+++ practice_parser/parser_python.py
@@ -1,48 +1,51 @@
-# TODO write a method such that:
-#       input: tree-sitter output
-#       output: 2D array with only letters, numbers, underscores
-
 import sys
 import re
 
-output_file = sys.argv[2]
-test_file = sys.argv[1]
-function = 'call'
-if '.java' in test_file:
-    function = 'invocation'
+class Parser:
 
-# output.py is the tree-sitter output for test.py
-code_lines = []
-with open(test_file, 'r') as test:
-    code_lines = [line.split('\n') for line in test] 
+    # determines function type
+    # returnes codelines
+    def codelines(self, test_file):
+        if '.java' in test_file:
+            function = 'invocation'
+        elif '.py' in test_file:
+            function = 'call'
+        # code_lines is an array where each element is a line of code
+        code_lines = []
+        with open(test_file, 'r') as test:
+            code_lines = [line.split('\n') for line in test] # this assumes method calls are oneline
+        return code_lines, function
 
-with open(output_file, 'r') as tree:
-    # Turns output.py into a 2D array organized by line
-    lines = [line.split() for line in tree]
+    # returns method calls from the output file
+    def getCalls(self, output_file, code_lines, function):
+        with open(output_file, 'r') as tree:
+            # Turns output.py into a 2D array organized by line
+            lines = [line.split() for line in tree]
 
-    # These are the character that are to be removed
-    replace_letters = ['[',']','-','(',')']
+            # An array with only tree-sitter identifiers and their code location indexes
+            new_lines = []
+            for line in lines:
+                new_lines.append(re.sub('[^A-Za-z0-9]', ' ', str(line)))
+
+            # final array containing the method calls
+            calls = []
+            for i in new_lines:
+                node = i.split()
+                for n in range(len(node)):
+                    if node[n].find(function) != -1: # true if the node contains a function call
+                        # appends corresponding call to the output array with indexes as integers
+                        calls.append(code_lines[int(node[n+1])][0][int(node[n+2]):int(node[n+4])]) 
+                        break
+            return calls
+
+
+def main():
+    parser = Parser()
+    output_file = sys.argv[2]
+    test_file = sys.argv[1]
     
-    # iterate through the 2D array of the output
-    # remove any characters that are not letters, underscores, or numbers
-    # returns same thing but cleaned up
-    new_lines = []
-    calls = []
-    for i in lines:
-        temp = re.sub('[^A-Za-z0-9]', ' ', str(i))
-        new_lines.append(temp)
-    for i in new_lines:
-        i = i.split()
-        for n in range(len(i)):
-            if i[n].find(function) != -1:
-                calls.append(code_lines[int(i[n+1])][0][int(i[n+2]):int(i[n+4])])
-                break
-    
-    # print statements
+    code_lines, function = parser.codelines(test_file)
+    print(parser.getCalls(output_file, code_lines, function))
 
-    #print(new_lines)     
-    #print(lines)
-    #print(len(lines[10]))
-    #print(lines[10])
-    print(calls)
-
+if __name__ == '__main__':
+    main()
