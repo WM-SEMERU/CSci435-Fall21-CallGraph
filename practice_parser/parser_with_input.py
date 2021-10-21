ac03f9b2ab7a3b36cf883745ab8316eff04e10f1
WillemMorrisWM <wmorris@email.wm.edu>
2021-10-20 23:13:30 -0400

    added method to retrieve class, method, and method call names from a file

--- practice_parser/parser_with_input.py
+++ practice_parser/parser_with_input.py
@@ -18,20 +18,52 @@ JAVA_LANGUAGE = Language('build/my-languages.so', 'java')  # In case people want
 parser = Parser()
 """----------------------"""
 
+# Every node in the tree has 4 attributes:
+# A type (like "function_definition" for methods, "expression_statement" for lines in a method, or "class_definition" for classes)
+# A start point which comes in the form of a tuple where the line number is index 0 and 
+#         the character number is index 1 (node.start_point[0] and node.start_point[1])
+# An end point that is formatted the same way as the start point (node.end_point).
+# A list of children (Using the tree-sitter playground is useful, the amount of indentation indicate a child-parent relationship).
+# 
+# There are also methods like get_sibling and get_parent and such but I haven't used any of those for parsing.
+
 # TODO method to parse python code
 def parse_python(file):
     parser.set_language(PY_LANGUAGE)
-    print("hello")
     src_code = file.read()
-    print(src_code)
     lines = src_code.split('\n')
     
     tree = parser.parse(bytes(src_code, "utf8"))
 
     root_node = tree.root_node
-    print(root_node)
+    classes, methods, calls = breath_search_tree(root_node, lines)
+    print(classes, end = '\n\n')
+    print(methods, end = '\n\n')
+    print(calls, end = '\n\n')
     
-  
+# The following code is a breath-first search of the tree by adding the children of all the nodes
+# currently in the children list to it.  It only terminates when there are no more children to add.
+def breath_search_tree(root_node,lines) -> list:
+    classes = []
+    methods = []
+    calls = []
+    children = root_node.children # initializes the list to parse
+    while len(children) > 0:
+      for child in children:
+        if child.type == 'class_definition':  # adds all class names to a list
+          name = child.children[1]
+          classes.append(lines[name.start_point[0]][name.start_point[1]:name.end_point[1]])
+        elif child.type == 'function_definition':  # adds all method names to a list
+          name = child.children[1]
+          methods.append(lines[name.start_point[0]][name.start_point[1]:name.end_point[1]])
+        elif child.type == 'expression_statement' and child.children[0].type == 'call':  # adds all method calls to a list
+          call_line = child.children[0]
+          calls.append(lines[call_line.start_point[0]][call_line.start_point[1]:call_line.end_point[1]])
+        if len(child.children) > 0:         # adds all this nodes children to the list of nodes to parse
+          children.extend(child.children)   # 
+        children.remove(child)              # removes the current node
+    return classes, methods, calls
+    
     
 # TODO method to parse java code
 def parse_java(src_code):   
@@ -44,29 +76,16 @@ def parse_java(src_code):
 # main method handles command line input and/or user input for the filepath and the language of the file to be parsed
 # Currently only supports parsing of 1 file at a time
 # command line arguments: [filepath] [language]
-def main():
-
-  cmd_line_args = sys.argv[1:] # cmd_line_args[0] = [filepath] and cmd_line_args[1] = [language]
-  
-  filepath = ""
-
-  if len(cmd_line_args) == 0:                                   # if there are no arguments passed to the command line take user input
-    filepath = input("What is the filepath of the file?" )
-    language = input("What language is the file coded in? ").lower()
-  elif len(cmd_line_args) == 1:                                 # if only the language is passed to the command line
-    filepath = cmd_line_args[0]
-    language = input("What language is the file coded in? ").lower()
-  else:                                                         # if both are passed to the command line
-    filepath = cmd_line_args[0]
-    language = cmd_line_args[1].lower()
-  
+def main():  
+  if len(sys.argv) == 1:                                   # if there are no arguments passed to the command line take user input
+    filepath = input("What is the filepath of the file? ")
+  else:
+    filepath = sys.argv[1]
   file = open(filepath, 'r')        # read-only file
 
-
-  # choose language to parse based on input
-  if language == 'python' or 'py':
+  if ".py" in filepath:
       parse_python(file)
-  elif language == 'java' or 'jv':
+  elif ".java" in filepath:
       parse_java(file)
 
 
