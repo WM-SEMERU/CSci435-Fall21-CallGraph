b09dc2d2124c5357cff661106c93ca728320ad64
TessieB <tabaumann@email.wm.edu>
2021-10-20 20:42:47 -0400

    added comments

--- practice_parser/parser_python.py
+++ practice_parser/parser_python.py
@@ -1,19 +1,29 @@
 import re
 import sys
 
+# Code file and output file from tree-sitter passed in from the command line
 output_file = sys.argv[2]
 test_file = sys.argv[1]
+
+# Python names for functions/calls based on tree-sitter output
 function = 'call'
 function2 = 'function'
 function2_2 = 'definition'
+
+# Java names for methods/calls based on tree-sitter output
 if '.java' in test_file:
 	function = 'invocation'
 	function2 = 'method'
 	function2_2 = 'declaration'
+
+# List of lines from original code file
 code_lines = []
 with open(test_file, 'r') as test:
     code_lines = [line.split('\n') for line in test]
 
+# Goes through tree-sitter output file and makes a dictionary
+# where the keys are method definitions and the values are
+# method calls for each key
 with open(output_file, 'r') as tree:
     # Turns output.py into a 2D array organized by line
     lines = [line.split() for line in tree]
@@ -30,9 +40,16 @@ with open(output_file, 'r') as tree:
         temp = re.sub('[^A-Za-z0-9]', ' ', str(i))
         new_lines.append(temp)
 
+    # Calls is a list of method calls, calls2 is a
+    # dictionary where the keys are method definitions and
+    # the values are method calls, last key signifies the last
+    # method definition that was parsed through
     calls = []
     calls2 = {}
     lastKey = ''
+
+    # Makes a dictionary where the keys are method
+    # definitions and the values are their method calls
     for i in new_lines:
         i = i.split()
         for n in range(len(i)):
@@ -45,18 +62,12 @@ with open(output_file, 'r') as tree:
                 lastKey = code_lines[int(i[n + 2])][0]
                 break
 
-    
-    # print statements
 
-    #print(new_lines)     
-    #print(lines)
-    #print(len(lines[10]))
+    # Prints out the method definitions and their calls
     for line in calls2:
-        print(line)
+        print(line.strip())
         print(calls2[line])
 
-    # for line in code_lines:
-    #     print(line)
 
 
 
