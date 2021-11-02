e9dba787a112620f95782369b81518d72ba4be50
WillemMorrisWM <wmorris@email.wm.edu>
2021-11-02 15:23:45 -0400

    added functionality to create vendor folder and clone git tree-sitter repos if not present

--- build_languages.py
+++ build_languages.py
@@ -1,6 +1,18 @@
 from tree_sitter import Language
+import os
+import git
 
 def main():
+    folder_path = os.path.dirname(__file__)+"\\vendor"
+    if not os.path.exists(folder_path):
+        os.mkdir(folder_path)
+    repository_path = folder_path+"\\tree-sitter-python"
+    if not os.path.exists(repository_path):
+        git.Repo.clone_from("https://github.com/tree-sitter/tree-sitter-python", repository_path)
+    repository_path = folder_path+"\\tree-sitter-java"
+    if not os.path.exists(repository_path):
+        git.Repo.clone_from("https://github.com/tree-sitter/tree-sitter-java", repository_path)
+    
     Language.build_library(
     'build/my-languages.so',
     [
