27e35be56c026d3e2c8977fda4d4010eeb021483
WillemMorrisWM <wmorris@email.wm.edu>
2021-11-14 15:54:53 -0500

    new

--- parser_with_input.py
+++ parser_with_input.py
@@ -1,4 +1,5 @@
-import sys
+import sys, os, argparse
+import git
 from pandas import DataFrame
 from tree_sitter import Language, Parser
 
@@ -11,8 +12,25 @@ lang = {
     "python": ["function_definition", "class_definition"],
     "java": ["method_declaration", "class_declaration"]
     }
+extensions = ['.py', '.java']
 
-# TODO method to parse python code
+method_df = {
+    'method': []
+}
+edge_df = {
+    'callee_index': [],
+    'called_index': [],
+    'call_index': []
+}
+
+src_path = ''
+
+# file dictionary:
+# key = filepath (so we can quickly open it)
+# values:
+# - the filepaths of the files it imports from the directory
+# - the start index of its methods in the methods csv
+# - the end index of its methods in the methods csv
 def parse_python():
     parser.set_language(PY_LANGUAGE)
     global tree
@@ -20,23 +38,45 @@ def parse_python():
 
     query = PY_LANGUAGE.query("""
     (function_definition) @method
-    (dotted_name) @import
+    (import_statement) @import
+    (import_from_statement) @import_from
     """)
     captures = query.captures(tree.root_node)
     method_nodes = [node[0] for node in captures if node[1] == 'method']
-    import_nodes = [node[0] for node in captures if node[1] == 'import']
-    methods = DataFrame({
-        'methods':[get_code_string(node.start_point, node.end_point) for node in method_nodes]
-        })
-    print(methods)
-    imports = DataFrame({
-        'imports':[get_code_string(node.start_point, node.end_point) for node in import_nodes]
-        })
-    print(imports)
-    
-    
+    import_nodes = [node[0] for node in captures if node[1] == 'import' or node[1] == 'import_from']
+    method_df['methods'].extend([node_to_string(node) for node in method_nodes])
 
-# TODO method to parse java code
+    imports = []
+    for imp in import_nodes:
+        file_to_search, name_to_search, thing_to_search = None, None, None
+        if imp.type == 'import_statement':
+            node = imp.child_by_field_name('name')
+            if node.type == 'aliased_import':
+                file_to_search = node_to_string(node.child_by_field_name('name'))
+                name_to_search = node_to_string(node.child_by_field_name('alias'))
+            else:
+                file_to_search = node_to_string(node)
+        elif imp.type == 'import_from_statement':
+            file_to_search = node_to_string(imp.child_by_field_name('module_name'))
+            node = imp.child_by_field_name('name')
+            if node.type == 'aliased_import':
+                thing_to_search = node_to_string(node.child_by_field_name('name'))
+                name_to_search = node_to_string(node.child_by_field_name('alias'))
+            else:
+                thing_to_search = node_to_string(node)
+        imports.append(tuple([file_to_search.replace('.',os.sep), thing_to_search, name_to_search]))
+
+    query = PY_LANGUAGE.query("""
+    (call) @call
+    (lambda) @lambda
+    """)
+    for node in method_nodes:
+        captures = query.captures(node)
+        call_nodes = [call[0] for call in captures if call[1] == 'call']
+        calls = [node_to_string(call) for call in call_nodes]
+
+
+
 def parse_java():
     parser.set_language(JAVA_LANGUAGE)
     global tree
@@ -44,16 +84,26 @@ def parse_java():
 
     query = JAVA_LANGUAGE.query("""
     (method_declaration) @method
+    (import_declaration) @import
     """)
-    methods = DataFrame({
-        'methods':[get_code_string(node[0].start_point, node[0].end_point) for node in query.captures(tree.root_node)]
-        })
-    print(methods)
+    captures = query.captures(tree.root_node)
+    method_nodes = [node[0] for node in captures if node[1] == 'method']
+    import_nodes = [node[0] for node in captures if node[1] == 'import']
+    method_df['methods'].extend([node_to_string(node) for node in method_nodes])
+    imports = []
+    for imp in import_nodes:
+        for child in imp.children[1:]:
+            if child.type == 'identifier' or child.type == 'scoped_identifier':
+                ident_str = node_to_string(child)
+                break
+        imports.append(ident_str.replace(".", os.sep))
 
 
 # Pre: the start_point and end_point values of a node
 # Post: return the substring of the source that the start_point and end_point point to
-def get_code_string(start_point, end_point) -> str:
+def node_to_string(node) -> str:
+    start_point = node.start_point
+    end_point = node.end_point
     if start_point[0] == end_point[0]:
         return lines[start_point[0]][start_point[1]:end_point[1]]
     ret = lines[start_point[0]][start_point[1]:] + "\n"
@@ -61,80 +111,61 @@ def get_code_string(start_point, end_point) -> str:
     ret += "\n" + lines[end_point[0]][:end_point[1]]
     return ret
 
-
-# This method uses the method call nodes to find their corresponding method parent nodes.
-# It then pairs them up in a dictionary/dataframe
-def print_method_dataframe_with_queries(query):
-    captures = query.captures(tree.root_node)     # captures should be a list of every method call in the file based on the query it is based on
-    method_dict = {}
-    method_dict['null'] = [[], 'null', 'null']
-    for capture in captures:
-        current_call = capture[0]
-
-        parent = current_call.parent
-        function, class_d = None, None
-        while parent.parent is not None:            # This loop finds the nearest function and class that the method call is a part of
-            if parent.type == lang[key][0]:
-                function = parent
-            elif parent.type == lang[key][1]: 
-                class_d = parent
-                break
-            parent = parent.parent                    # iterate through the method call's parents
-    
-        call_name = get_code_string(current_call.start_point, current_call.end_point)  # name of the method call
-
-        if parent.parent is None and function is None and class_d is None:                   # method is a global variable
-            method_calls = method_dict['null'][0]
-            method_calls.append(call_name)
-            method_dict['null'][0] = method_calls      # update global method_calls
-            continue
-
-        if function is not None:        # finds the function name if it is there
-            name_node = function.child_by_field_name('name')    
-            parameters_node = function.child_by_field_name('parameters')
-            function_name = get_code_string(name_node.start_point, name_node.end_point) + get_code_string(parameters_node.start_point, parameters_node.end_point)
-            function_definition = get_code_string(function.start_point, function.end_point)
-        else:
-            function_name = 'null'
-            function_definition = 'null'
-        if class_d is not None:         # finds the class name if it is there
-            name_node = class_d.child_by_field_name('name')
-            class_name = get_code_string(name_node.start_point, name_node.end_point)
-        else:
-            class_name = 'null'
-
-        if function_name in method_dict.keys():         # Since the function is already in the dataframe/dictionary, get the list of method calls, 
-            method_calls = method_dict[function_name][0] # append the current method call, and reinitialize the method call list
-            method_calls.append(call_name)
-            method_dict[function_name][0] = method_calls
-        else:                                           # function is not in the dataframe/dictionary, initialize its list of method calls
-            method_dict[function_name] = [[call_name], class_name, function_definition]    # making it a list of lists solve the problem with the lists being of different lengths
-
-
-    df = DataFrame(method_dict)  
-
-    print(df.to_csv(sep = ','))   # csv may be annoying to look at since method defintion takes up a lot of space
-
 def parse_file(filepath):
+    src_path = filepath
+    try:
+        file = open(src_path, 'r', encoding='utf-8')
+    except FileNotFoundError:
+        print('Could not open file: %s\nExiting...' % src_path)
+        sys.exit(1)
     global lines, src_code, key
-    with open(filepath, 'r', encoding='utf-8') as file:
-        src_code = file.read()
-        lines = src_code.split('\n')
-        if filepath.endswith(".py"):
-            key = 'python'
-            parse_python()
-        elif filepath.endswith(".java"):
-            key = 'java'
-            parse_java()
+    src_code = file.read()
+    lines = src_code.split('\n')
+    if filepath.endswith(".py"):
+        key = 'python'
+        parse_python()
+    elif filepath.endswith(".java"):
+        key = 'java'
+        parse_java()
+    
+
+def parse_directory(dir_path):
+    src_path = dir_path
+    if not os.path.isdir(src_path):
+        print('Could not find directory: %s\nExiting...' % src_path)
+        sys.exit(1)
+    for subdir,dir,files in os.walk(src_path):
+        for filename in files:
+            filepath = os.path.join(subdir,filename)
+            if filename.endswith('.py'):
+                parse_file(filepath)
+            elif filename.endswith('.java'):
+                parse_file(filepath)
+
+def parse_repo(link):
+    repo_name = link.split('/')[-1].replace('.git','')
+    repo_path = os.path.join(os.path.dirname(__file__), repo_name)
+    if not os.path.exists(repo_path):
+        git.Repo.clone_from(link, repo_path)
+    parse_directory(repo_path)
+
+argparser = argparse.ArgumentParser(description='interpret type of parsing')
+argparser.add_argument('-f', '--file')
+argparser.add_argument('-d', '--directory')
+argparser.add_argument('-r', '--repository')
 
 def main():
-    if len(sys.argv) == 1:                                    # if there are no arguments passed to the command line take user input
-        filepath = input("What is the filepath of the file? ")
-    else:
-        filepath = sys.argv[1]                                  # take command line input for file to parse
-
-    parse_file(filepath)
-
+    args = argparser.parse_args(sys.argv[1:])
+    print(args)
+    
+    if args.file is not None:
+        parse_file(args.file)
+    elif args.directory is not None:
+        parse_directory(args.directory)
+    elif args.repository is not None:
+        parse_repo(args.repository)
+    print(method_df)
+    print(edge_df)
 
 if __name__ == '__main__':
     main()
