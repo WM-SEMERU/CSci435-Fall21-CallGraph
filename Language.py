85527fdea1d30f2594f78d465f0f333b24808ffb
spencer-bao <spencerbao@gmail.com>
2021-11-15 11:07:12 -0500

    extended functionality to java language

--- Language.py
+++ Language.py
@@ -4,5 +4,10 @@ class Language:
         if language == "python":
             self.language = "python"
             self.extension = ".py"
-            self.language_library = Language("build/my-languages.so", "python)")
+            self.language_library = Language("build/my-languages.so", "python")
             self.terminology = ["function_definition", "class_definition"]
+        elif language == "java":
+            self.language = "java"
+            self.extension = ".java"
+            self.language_library = Language("build/my-languages.so", "java")
+            self.terminology = ["method_declaration", "class_declaration"]
