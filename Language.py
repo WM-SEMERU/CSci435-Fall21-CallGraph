4962fbe64e39e697c7c6729be24d0b6543e82491
spencer-bao <spencerbao@gmail.com>
2021-11-16 14:57:28 -0500

    categorized different queries

--- Language.py
+++ Language.py
@@ -1,34 +1,38 @@
 from tree_sitter import Language, Parser
-
-from parser_with_input import parse_python
 class Language:
     def __init__(self, language):
         if language == "python":
             self.language = "python"
             self.extension = ".py"
             self.language_library = Language("build/my-languages.so", "python")
-            self.terminology = ["function_definition", "class_definition"]
-            self.query = self.language_library.query("""
+            self.method_import_q = self.language_libary.query("""
                 (function_definition) @method
                 (import_statement) @import
-                (import_from_statement) @import_from
-                (call) @call
-                (lambda) @lambda 
+                (import_from_statement) @import
                 """)
+            self.call_q = self.language_library.query("""
+                (call) @call
+                (lambda) @lambda
+                """)
+
         elif language == "java":
             self.language = "java"
             self.extension = ".java"
             self.language_library = Language("build/my-languages.so", "java")
-            self.terminology = ["method_declaration", "class_declaration"]
-            self.query = self.language_library.query("""
+            self.method_import_q = self.language_library.query("""
                 (method_declaration) @method
-                (import_declaration) @import
-                """)    
+                (constructor_declaration) @method
+                (import_declaration) @import 
+                """)
+            self.call_q = self.language_library.query("""
+                (method_invocation) @method
+                (object_creation_expression) @method
+                """)  
 
     def parse(self, src_code, method_nodes, import_nodes):
         parser = Parser()
         parser.set_language(self.language_library)
         tree = self.parser.parse(bytes(src_code, "utf8"))
-        captures = self.query.captures(tree.root_node)
+        captures = self.method_import_q.captures(tree.root_node)
         method_nodes = [node[0] for node in captures if node[1] == 'method']
         import_nodes = [node[0] for node in captures if node[1] == 'import' or node[1] == 'import_from']
