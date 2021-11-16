1bd88b2d26b34e64585ca9da9455ea72707e6c4e
WillemMorrisWM <wmorris@email.wm.edu>
2021-11-16 16:13:39 -0500

    major refactoring of parse_directory and parse_file

--- parser_with_input.py
+++ parser_with_input.py
@@ -3,101 +3,50 @@ import git
 from pandas import DataFrame
 from tree_sitter import Language, Parser
 
-PY_LANGUAGE = Language('build/my-languages.so', 'python')
-JAVA_LANGUAGE = Language('build/my-languages.so', 'java') 
+class LanguageData:
+    def __init__(self, language):
+        self.PARSER = Parser()
+        if language == 'python' or language == 'py':
+            self.language = 'python'
+            self.extension = ".py"
+            self.language_library = Language('build/my-languages.so', 'python')
+            self.PARSER.set_language(self.language_library)
+            self.method_import_q = self.language_library.query("""
+            (function_definition) @method
+            (import_statement) @import
+            (import_from_statement) @import
+            """)
+            self.call_q = self.language_library.query("""
+            (call) @call
+            (lambda) @lambda
+            """)
+        elif language == 'java' or language == 'jv':
+            self.language = 'java'
+            self.extension = ".java"
+            self.language_library = Language('build/my-languages.so', 'java') 
+            self.PARSER.set_language(self.language_library)
+            self.method_import_q = self.language_library.query("""
+            (method_declaration) @method
+            (constructor_declaration) @method
+            (import_declaration) @import 
+            """)
+            self.call_q = self.language_library.query("""
+            (method_invocation) @method
+            (object_creation_expression) @method
+            """)
+        else:
+            raise ValueError("Unknown language: %s" % language)
 
-parser = Parser()
-
-lang = {
-    "python": ["function_definition", "class_definition"],
-    "java": ["method_declaration", "class_declaration"]
-    }
-extensions = ['.py', '.java']
-
-method_df = {
+method_dict = {
     'method': []
 }
-edge_df = {
+method_nodes = []
+file_dict = {}
+edge_dict = {
     'callee_index': [],
     'called_index': [],
-    'call_index': []
+    'call_line': []
 }
-
-src_path = ''
-
-# file dictionary:
-# key = filepath (so we can quickly open it)
-# values:
-# - the filepaths of the files it imports from the directory
-# - the start index of its methods in the methods csv
-# - the end index of its methods in the methods csv
-def parse_python():
-    parser.set_language(PY_LANGUAGE)
-    global tree
-    tree = parser.parse(bytes(src_code, "utf8"))
-
-    query = PY_LANGUAGE.query("""
-    (function_definition) @method
-    (import_statement) @import
-    (import_from_statement) @import_from
-    """)
-    captures = query.captures(tree.root_node)
-    method_nodes = [node[0] for node in captures if node[1] == 'method']
-    import_nodes = [node[0] for node in captures if node[1] == 'import' or node[1] == 'import_from']
-    method_df['methods'].extend([node_to_string(node) for node in method_nodes])
-
-    imports = []
-    for imp in import_nodes:
-        file_to_search, name_to_search, thing_to_search = None, None, None
-        if imp.type == 'import_statement':
-            node = imp.child_by_field_name('name')
-            if node.type == 'aliased_import':
-                file_to_search = node_to_string(node.child_by_field_name('name'))
-                name_to_search = node_to_string(node.child_by_field_name('alias'))
-            else:
-                file_to_search = node_to_string(node)
-        elif imp.type == 'import_from_statement':
-            file_to_search = node_to_string(imp.child_by_field_name('module_name'))
-            node = imp.child_by_field_name('name')
-            if node.type == 'aliased_import':
-                thing_to_search = node_to_string(node.child_by_field_name('name'))
-                name_to_search = node_to_string(node.child_by_field_name('alias'))
-            else:
-                thing_to_search = node_to_string(node)
-        imports.append(tuple([file_to_search.replace('.',os.sep), thing_to_search, name_to_search]))
-
-    query = PY_LANGUAGE.query("""
-    (call) @call
-    (lambda) @lambda
-    """)
-    for node in method_nodes:
-        captures = query.captures(node)
-        call_nodes = [call[0] for call in captures if call[1] == 'call']
-        calls = [node_to_string(call) for call in call_nodes]
-
-
-
-def parse_java():
-    parser.set_language(JAVA_LANGUAGE)
-    global tree
-    tree = parser.parse(bytes(src_code, "utf8"))
-
-    query = JAVA_LANGUAGE.query("""
-    (method_declaration) @method
-    (import_declaration) @import
-    """)
-    captures = query.captures(tree.root_node)
-    method_nodes = [node[0] for node in captures if node[1] == 'method']
-    import_nodes = [node[0] for node in captures if node[1] == 'import']
-    method_df['methods'].extend([node_to_string(node) for node in method_nodes])
-    imports = []
-    for imp in import_nodes:
-        for child in imp.children[1:]:
-            if child.type == 'identifier' or child.type == 'scoped_identifier':
-                ident_str = node_to_string(child)
-                break
-        imports.append(ident_str.replace(".", os.sep))
-
 
 # Pre: the start_point and end_point values of a node
 # Post: return the substring of the source that the start_point and end_point point to
@@ -111,34 +60,66 @@ def node_to_string(node) -> str:
     ret += "\n" + lines[end_point[0]][:end_point[1]]
     return ret
 
+def add_methods_and_imports(filepath):
+    tree = lang.PARSER.parse(bytes(src_code, "utf8"))
+    query = lang.method_import_q
+    captures = query.captures(tree.root_node)
+    ## adds all the method nodes to a list and all the method definition to a dictionary
+    cur_method_nodes = [node[0] for node in captures if node[1] == 'method']
+
+    method_dict['method'].extend([node_to_string(node) for node in cur_method_nodes])
+    method_nodes.extend(cur_method_nodes)
+    ## adds all files that the file imports to a list and the range of indexes in the method dictionary that point to that file
+    import_nodes = [node[0] for node in captures if node[1] == 'import']
+
+    file_list = [filepath]
+    for imp in import_nodes:
+        file_to_search = None
+        if lang.language == 'python':
+            node = imp.child_by_field_name('name')
+            if imp.type == 'import_statement':
+                if node.type == 'aliased_import':
+                    file_to_search = node_to_string(node.child_by_field_name('name'))
+                else:
+                    file_to_search = node_to_string(node)
+            elif imp.type == 'import_from_statement':
+                file_to_search = node_to_string(imp.child_by_field_name('module_name'))
+        elif lang.language == 'java':
+            for child in imp.children[1:]:
+                if child.type == 'identifier' or child.type == 'scoped_identifier':
+                    file_to_search = node_to_string(child)
+                    break
+        import_path = os.path.join(os.path.dirname(filepath), file_to_search.replace(".", os.sep) + lang.extension)
+        if os.path.exists(import_path):
+            file_list.append(import_path)
+    file_dict[filepath] = [file_list, (len(method_nodes) - len(cur_method_nodes), len(method_nodes))]
+
+def add_edges(filepath):
+    pass
+
 def parse_file(filepath):
-    src_path = filepath
     try:
-        file = open(src_path, 'r', encoding='utf-8')
+        file = open(filepath, 'r', encoding='utf-8')
     except FileNotFoundError:
-        exit_with_message('Could not open file: %s' % src_path)
-    global lines, src_code, key
+        exit_with_message('Could not open file: %s' % filepath)
+    global lines, src_code
     src_code = file.read()
     lines = src_code.split('\n')
-    if filepath.endswith(".py"):
-        key = 'python'
-        parse_python()
-    elif filepath.endswith(".java"):
-        key = 'java'
-        parse_java()
-    
 
 def parse_directory(dir_path):
     src_path = dir_path
     if not os.path.isdir(src_path):
         exit_with_message('Could not find directory: %s' % src_path)
-    for subdir,dir,files in os.walk(src_path):
+    walk = os.walk(src_path)
+    for subdir,dir,files in walk:
         for filename in files:
             filepath = os.path.join(subdir,filename)
-            if filename.endswith('.py'):
+            if filename.endswith(lang.extension):
                 parse_file(filepath)
-            elif filename.endswith('.java'):
-                parse_file(filepath)
+                add_methods_and_imports(filepath)
+    for filepath in file_dict.keys():
+        parse_file(filepath)
+        add_edges(filepath)
 
 def parse_repo(link):
     repo_name = link.split('/')[-1].replace('.git','')
@@ -163,17 +144,13 @@ def exit_with_message(message):
 
 def main():
     args = argparser.parse_args(sys.argv[1:])
-    print(args)
-    
-    if args.language == 'python' or args.language == 'py':
-        pass
-    elif args.language == 'java' or args.language == 'jv':
-        pass
-    else:
-        exit_with_message("No language specified or the language is not supported. Exiting...")
+    global lang
+    lang = LanguageData(args.language)
 
     if args.file is not None:
         parse_file(args.file)
+        add_methods_and_imports(args.file)
+        add_edges(args.file)
     elif args.directory is not None:
         parse_directory(args.directory)
     elif args.repository is not None:
@@ -181,7 +158,9 @@ def main():
     else:
         exit_with_message("No File, Directory, or Repository passed as argument.")
     
+    method_df = DataFrame(method_dict)
     print(method_df)
+    edge_df = DataFrame(edge_dict)
     print(edge_df)
 
 if __name__ == '__main__':
