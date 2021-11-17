353eb3da2f9a7b41076e97f99175afdeef596e48
WillemMorrisWM <wmorris@email.wm.edu>
2021-11-17 18:45:19 -0500

    moved LanguageData to its own class

--- parser_with_input.py
+++ parser_with_input.py
@@ -2,40 +2,7 @@ import sys, os, argparse
 import git
 from pandas import DataFrame
 from tree_sitter import Language, Parser
-
-class LanguageData:
-    def __init__(self, language):
-        self.PARSER = Parser()
-        if language == 'python' or language == 'py':
-            self.language = 'python'
-            self.extension = ".py"
-            self.language_library = Language('build/my-languages.so', 'python')
-            self.PARSER.set_language(self.language_library)
-            self.method_import_q = self.language_library.query("""
-            (function_definition) @method
-            (import_statement) @import
-            (import_from_statement) @import
-            """)
-            self.call_q = self.language_library.query("""
-            (call) @call
-            (lambda) @lambda
-            """)
-        elif language == 'java' or language == 'jv':
-            self.language = 'java'
-            self.extension = ".java"
-            self.language_library = Language('build/my-languages.so', 'java') 
-            self.PARSER.set_language(self.language_library)
-            self.method_import_q = self.language_library.query("""
-            (method_declaration) @method
-            (constructor_declaration) @method
-            (import_declaration) @import 
-            """)
-            self.call_q = self.language_library.query("""
-            (method_invocation) @method
-            (object_creation_expression) @method 
-            """)
-        else:
-            raise ValueError("Unknown language: %s" % language)
+import LanguageData
 
 method_dict = {
     'method': []
