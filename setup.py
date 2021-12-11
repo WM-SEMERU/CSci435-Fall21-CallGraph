a8241c93f87edfc8eaed2590f5729efe9d11716b
WillemMorrisWM <wmorris@email.wm.edu>
2021-12-11 18:33:18 -0500

    uploaded a version of the package to test pypi

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
 import setuptools
 
 setuptools.setup(
-    name='call_graph_generator',
-    version='0.0.1',
-    author='',
-    author_email='',
-    description='a package to generate call graphs',
+    name='call_graphs_with_gnn',
+    version='0.1.0',
+    author='Call Graph',
+    author_email='nacooper01@email.wm.edu',
+    description='a package to generate call graphs and train them on graph neural networks',
     long_description=open('README.md', 'r', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
     license='MIT',
-    url='git@github.com:WM-SEMERU/CSci435-Fall21-CallGraph.git',
+    url='https://github.com/WM-SEMERU/CSci435-Fall21-CallGraph.git',
     install_requires=[
         'tree-sitter >= 0.19.0',
         'pandas >= 1.3.3',
         'gitpython >= 3.1.24'
     ],
-	python_requires=">=3.9",
-	packages=['src', 'parsers'],
-    package_dir={'src': 'src', 'parsers': 'src/parsers'},
+	packages=['call_graphs_with_gnn'],
     classifiers=[
         'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
     ]
 )
